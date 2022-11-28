f97e7d5b7d340f23a87c722d07865cf7ed4539b9
vitorPassos <vitorpassosbrit@gmail.com>
2022-11-28 16:10:13 -0300

    ultima aula

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+app_name = "cliente"
 urlpatterns = [
     path("", view=views.index, name="index"),
-    path("lista",views.lista),
-    path("lista/<int:id>/",views.detalhe)
+    path("lista",views.lista , name="lista"),
+    path("lista/<int:id>/",views.detalhe),
+    path("contatos/",views.contato)
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -1,32 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Cliente
-# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world!")
+    if request.method == "POST":
+        nome = request.POST["nome"]
+        cpf_cnpj = request.POST["cpf"]
+        tipo = request.POST["tipo"]
 
+        cliente = {
+            "nome":nome,
+            "cpf" :cpf_cnpj,
+            "tipo": tipo
+        }
+        return redirect(to="cliente:list", context={"cliente" : cliente})
+    else:
+        return render(request,"index.html")
 def lista(request):
     clientes = Cliente.objects.all()
     return render(request, "lista.html",
     context={"clientes":clientes, 
     "titulo":"Lista de clientes"})
-    
 def detalhe(request,id):
     cliente = get_object_or_404(Cliente, id=id)
     return render(request, "detalhe.html" , context={"cliente":cliente})
+def contato(request):
+    return render(request,"contatos.html")
 
-
-
-predios = [
-    {
-       "nome":"exemplo" 
-    },
-    {
-        "nome":"exemplo" 
-    },
-    {
-        "nome":"exemplo" 
-    }
-
-]
